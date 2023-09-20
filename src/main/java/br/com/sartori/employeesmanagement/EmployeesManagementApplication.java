b2c8c22ea890d3bb6bf64e12ff1d017037bb714f
Rodrigo Sartori <93sartori@gmail.com>
2023-09-20 18:55:49 -0300

    First Commit

---
+++ src/main/java/br/com/sartori/employeesmanagement/EmployeesManagementApplication.java
@@ -0,0 +1,13 @@
+package br.com.sartori.employeesmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sartori/employeesmanagement/EmployeesManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sartori.employeesmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
